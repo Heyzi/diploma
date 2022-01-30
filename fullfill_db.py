b2f56d67c95f1f5db808a34fa471154c596147e3
A.Platonnova <planol@me.com>
2022-01-30 15:45:24 +0000

    add db filler and error fixes

---
+++ fullfill_db.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from db import create_db, insert_to_table
+from datetime import date, timedelta
+from backend import weather_for_every_day, weather_to_db
+
+def list_dates():
+    start_date = date(2014, 5, 31)
+    end_date = date.today()
+
+    delta = end_date - start_date
+    datelist = []    
+    for i in range(delta.days + 1):
+       day = start_date + timedelta(days=i)
+       day = day.strftime("%Y/%m/%d")
+       print(day)
+       insert_to_table(weather_for_every_day(day))
+       print("added to db")
+    return day
+print(list_dates())
+
--- run.py
+++ run.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
 import requests
 from flask import Flask, render_template, redirect, request
-from requests.exceptions import HTTPError
 from db import user_select
 import time
 from backend import weather_to_db, userdate_weather_to_db
 
-#result = select_all()
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    date = time.strftime("%Y-%m")
     result = user_select(time.strftime("%Y-%m"))
-    return render_template("index.html", data=result)
+    return render_template("index.html", data=result, cur_month=date)
 
 @app.route('/upd', methods=['POST'])
 def upd():
     userdate = request.form["update_month"]
-    userdate_weather_to_db(userdate)
-    result2 = user_select(userdate)
+    if request.form['action'] == 'update':
+     userdate_weather_to_db(userdate)
+     result2 = user_select(userdate)
+    else:
+     user_select(userdate)
+     result2 = user_select(userdate)
 
-    return render_template("index.html", data=result2)
+    return render_template("index.html", data=result2, cur_month=userdate)
+
+@app.errorhandler(500)
+def internal_error(error):
+
+    return render_template("500.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
