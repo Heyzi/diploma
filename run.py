ade816eef3e760e931eb0b794582259f1fa9b883
Alexandr Platonov <planol@me.co>
2022-02-03 20:45:26 +0300

    ui fixes

--- run.py
+++ run.py
@@ -36,7 +36,11 @@ def index():
 
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template("500.html")
+    return render_template("500.html"), 500
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
 
 if __name__ == "__main__":
     app.secret_key = 'qwerty'
