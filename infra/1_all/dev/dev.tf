1e8b2b7a8752ab712363ca5ab8685bc921f8d2ff
Alexandr Platonov <planol@me.co>
2022-04-01 21:03:18 +0300

    fix

--- infra/1_all/dev/dev.tf
+++ infra/1_all/dev/dev.tf
@@ -1,26 +1,32 @@
+locals {
+  production_availability_zones = ["${var.AWS_REGION}a", "${var.AWS_REGION}b", "${var.AWS_REGION}c"]
+}
+
 module "default-vpc" {
   source           = "../modules/vpc"
   ENV              = var.ENV
   CIDR_BLOCK       = var.CIDR_BLOCK
 }
 
-module "instance" {
-  source         = "../modules/instance"
-  ENV            = var.ENV
-  VPC_ID         = module.default-vpc.vpc_id
-  PUB_KEY        = var.PUB_KEY
-  INSTANCE_TYPE  = var.INSTANCE_TYPE
-  EGRESS_RULES   = var.EGRESS_RULES
-  INGRESS_RULES  = var.INGRESS_RULES
-  SUBNET_ID      = module.network.subnet_id
-}
-
 module "network" {
   source         = "../modules/network"
-  ENV            = var.ENV
-  VPC_ID         = module.default-vpc.vpc_id
-  DEF_RT         = module.default-vpc.default_rt_id
-  AZ             = var.AZ
-  CIDR_BLOCK     = var.CIDR_BLOCK
+  region               = var.AWS_REGION
+  environment          = var.ENV
+
+
+  # VPC_ID         = module.default-vpc.vpc_id
+  # DEF_RT         = module.default-vpc.default_rt_id
+  # AZ             = var.AZ
+  # CIDR_BLOCK     = var.CIDR_BLOCK
+
+}
+
+module "Networking" {
+
+  
+  vpc_cidr             = var.vpc_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
+  private_subnets_cidr = var.private_subnets_cidr
+  availability_zones   = local.production_availability_zones
 
 }
--- infra/1_all/modules/network/network.tf
+++ infra/1_all/modules/network/network.tf
@@ -1,16 +1,12 @@
 terraform {
   required_version = ">= 0.12"
 }
-variable "ENV" {
-}
-variable "VPC_ID" {
-}
-variable "DEF_RT" {
-}
-variable "CIDR_BLOCK" {
-}
-variable "AZ" {
-}
+variable "ENV" {}
+variable "VPC_ID" {}
+variable "DEF_RT" {}
+variable "CIDR_BLOCK" {}
+variable "SUBNETS_CIDR" {}
+variable "AZ" {}
 
 resource "aws_internet_gateway" "default_igw" {
   vpc_id = var.VPC_ID
@@ -31,18 +27,17 @@ resource "aws_default_route_table" "default_route_table" {
   }
 }
 
-resource "aws_subnet" "main_subnet" {
-  vpc_id                  = var.VPC_ID
-  cidr_block              = var.CIDR_BLOCK
-  availability_zone       = var.AZ
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  count                   = length(var.public_subnets_cidr)
+  cidr_block              = element(var.public_subnets_cidr, count.index)
+  availability_zone       = element(var.AZ, count.index)
+#  map_public_ip_on_launch = true
 
   tags = {
-    Name = "${var.ENV}-subnet"
+    Name        = "${var.ENV}-${element(var.AZ, count.index)}-subnet"
+    Environment = "${var.ENV}"
   }
 }
 
-output "subnet_id" {
-  description = "Subnet id"
-  value       = aws_subnet.main_subnet.id
-}
 
